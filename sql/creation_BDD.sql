f6c3704678e0305722703cbe6d712f9a81987358
NagromLeBg <morganrichard9000@gmail.com>
2026-04-27 12:32:22 +0200

    mofication architecture BDD

--- sql/creation_BDD.sql
+++ sql/creation_BDD.sql
@@ -2,12 +2,14 @@
 CREATE DATABASE romans_nlp;
 USE romans_nlp;
 
+-- Création des tables pour la base de données des romans et auteurs
 CREATE TABLE auteurs (
     id_auteur INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(100) NOT NULL,
     prenom VARCHAR(100)
 );
 
+-- Table pour stocker les romans, avec leur titre et année de publication
 CREATE TABLE romans (
     id_roman INT PRIMARY KEY AUTO_INCREMENT,
     titre VARCHAR(255) NOT NULL,
@@ -15,6 +17,7 @@ CREATE TABLE romans (
     texte_brut LONGTEXT
 );
 
+-- Table d'association pour gérer la relation plusieurs-à-plusieurs entre auteurs et romans
 CREATE TABLE auteurs_romans (
     id_auteur INT NOT NULL,
     id_roman INT NOT NULL,
@@ -23,6 +26,7 @@ CREATE TABLE auteurs_romans (
     FOREIGN KEY (id_roman) REFERENCES romans(id_roman)
 );
 
+-- Table pour stocker les segments extraits des romans, avec leur ordre de segmentation
 CREATE TABLE segments (
     id_segment INT PRIMARY KEY AUTO_INCREMENT,
     id_roman INT NOT NULL,
@@ -31,9 +35,12 @@ CREATE TABLE segments (
     FOREIGN KEY (id_roman) REFERENCES romans(id_roman)
 );
 
-CREATE TABLE segments_lemmatises (
-    id_segment_lemmatise INT PRIMARY KEY AUTO_INCREMENT,
-    id_segment INT NOT NULL,
-    contenu_lemmatise LONGTEXT NOT NULL,
-    FOREIGN KEY (id_segment) REFERENCES segments(id_segment)
+-- Table pour stocker les phrases extraites des romans, avec leur motif d'extraction
+CREATE TABLE phrase_roman (
+id_phrase INT PRIMARY KEY AUTO_INCREMENT,
+id_roman INT NOT NULL,
+ordre_phrase INT NOT NULL,
+texte_phrase TEXT NOT NULL,
+motif_phrase TEXT NOT NULL,
+FOREIGN KEY (id_roman) REFERENCES romans(id_roman)
 );
