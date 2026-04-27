b25d5e9a2462fa8688a1918851326476c9332f9f
NagromLeBg <morganrichard9000@gmail.com>
2026-04-27 16:21:46 +0200

    .

--- sql/commande_de_base.sql
+++ sql/commande_de_base.sql
@@ -1,16 +1,15 @@
+-- Commandes pour utiliser la base de données romans_nlp
 USE romans_nlp;
 
-# voir tous les auteurs de la base de données
+-- voir tous les auteurs de la base de données
 SELECT * FROM auteurs;  
 
-# pour voir les auteurs associés à chaque roman avec les titres et les années de publication
+-- pour voir les auteurs associés à chaque roman avec les titres et les années de publication
 SELECT a.prenom, a.nom, r.titre, r.annee_publication
 FROM auteurs a
 JOIN auteurs_romans ar ON a.id_auteur = ar.id_auteur
 JOIN romans r ON ar.id_roman = r.id_roman
 ORDER BY r.annee_publication, a.prenom;
-
-SELECT * FROM romans;
 
 SELECT texte_segment FROM segments
 WHERE id_roman=2;
